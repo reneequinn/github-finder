29867806b392c7a88f194f87daca6fb139d12ad5
reneequinn <reneequinn17@gmail.com>
2020-08-17 15:15:15 +0930

    Adds clear button and functionality to clear the search results

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,6 +8,8 @@ class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
 
   onChange = (e) => {
@@ -23,6 +25,7 @@ class Search extends Component {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
@@ -39,6 +42,11 @@ class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
